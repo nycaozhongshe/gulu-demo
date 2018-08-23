955c07766d9da56352a2f47f17734816376987c1
nytomato <czs1037976103@gmail.com>
2018-08-24 00:04:45 +0800

    init(all): init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # gulu-demo
-gulu-demo
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<!--  -->
+<template>
+  <div>
+    <button class="g-button"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  created() {},
+
+  methods: {}
+};
+</script>
+<style lang='scss' scoped>
+.g-button {
+}
+</style>
